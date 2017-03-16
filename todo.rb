ebc90256b835efebc486748a0344dce300f9bc45
Daniel Vopalecky <daniel.vopalecky@gmail.com>
2017-03-17 00:54:22 +0100

    add page for single todo

--- todo.rb
+++ todo.rb
@@ -23,23 +23,39 @@ get '/lists' do
   erb :lists
 end
 
+# Render the new list form
+get '/lists/new' do
+  erb :new_list
+end
+
+get '/lists/:number' do
+  @list = session[:lists][params[:number].to_i]
+  if @list
+    erb :list
+  else
+    redirect '/lists'
+  end
+end
+
+# Return an error message if list name invalid, otherwise return nil
+def error_for_list_name(name)
+  if !(1..100).cover?(name.size)
+    'List name must be 1 to 100 characters long!'
+  elsif session[:lists].map { |v| v[:name] }.include?(name)
+    'List name must be unique!'
+  end
+end
+
 # Create a new list
 post '/lists' do
   list_name = params[:list_name].strip
-  if !(1..100).cover?(list_name.size)
-    session[:error] = "List name must be 1 to 100 characters long!"
-    erb :new_list
-  elsif session[:lists].map{ |v| v[:name] }.include?(list_name)
-    session[:error] = "List name must be unique!"
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
     erb :new_list
   else
     session[:lists] << { name: list_name, todos: [] }
     session[:success] = "List #{list_name} created successfully."
     redirect '/lists'
   end
-end
-
-# Render the new list form
-get '/lists/new' do
-  erb :new_list
 end
