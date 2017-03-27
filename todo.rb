88117f0fb36ed88089dce210cd087794cab0c352
Daniel Vopalecky <daniel.vopalecky@gmail.com>
2017-03-27 17:38:43 +0200

    Added: View all todos for a list, edit list, delete list

--- todo.rb
+++ todo.rb
@@ -2,6 +2,7 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/content_for'
 require 'tilt/erubis'
 
 configure do
@@ -28,15 +29,6 @@ get '/lists/new' do
   erb :new_list
 end
 
-get '/lists/:number' do
-  @list = session[:lists][params[:number].to_i]
-  if @list
-    erb :list
-  else
-    redirect '/lists'
-  end
-end
-
 # Return an error message if list name invalid, otherwise return nil
 def error_for_list_name(name)
   if !(1..100).cover?(name.size)
@@ -57,5 +49,50 @@ post '/lists' do
     session[:lists] << { name: list_name, todos: [] }
     session[:success] = "List #{list_name} created successfully."
     redirect '/lists'
+  end
+end
+
+# View all todos for given list
+get '/lists/:id' do
+  @id = params[:id].to_i
+  @list = session[:lists][@id]
+  if @list
+    erb :list
+  else
+    session[:error] = "The specified list was not found."
+    redirect '/lists'
+  end
+end
+
+# Edit existing list
+get '/lists/:id/edit' do
+  @id = params[:id].to_i
+  @list = session[:lists][@id]
+  erb :edit_list
+end
+
+# Delete existing list
+post '/lists/:id/delete' do
+  id = params[:id].to_i
+  @list = session[:lists].delete_at(id)
+  session[:success] = "List #{@list[:name]} deleted successfully."
+  redirect "/lists"
+end
+
+# Update existing list
+post '/lists/:id' do
+  @id = params[:id].to_i
+  @list = session[:lists][@id]
+
+  edited_list_name = params[:list_name].strip
+  error = error_for_list_name(edited_list_name)
+  if error
+    session[:error] = error
+
+    erb :edit_list
+  else
+    @list[:name] = edited_list_name
+    session[:success] = "List #{edited_list_name} renamed successfully."
+    redirect "/lists/#{@id}"
   end
 end
